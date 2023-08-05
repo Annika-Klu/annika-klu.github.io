cba13c4d0edf78d2836d0152faafec8efd9a8235
Klüpfel Annika <AnnikaKluepfel@web.de>
2023-08-05 21:10:53 +0200

    update home view

---
+++ src/components/GoUp.vue
@@ -0,0 +1,12 @@
+<template>
+    <a class="line top dark" href="#landing" >^</a>
+</template>
+
+<style scoped>
+a {
+  position: absolute;
+  font-size: 2.5rem;
+  bottom: 1rem;
+  right: 2rem;
+}
+</style>
