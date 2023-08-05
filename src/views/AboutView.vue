4dce6554104246be2bdb3441e40efb138d63656c
Klüpfel Annika <AnnikaKluepfel@web.de>
2023-08-05 21:11:49 +0200

    simple 'about' page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,23 @@
 <script setup>
-import ExampleComponent from "../components/ExampleComponent.vue";
+import BasePage from "../components/BasePage.vue"
 </script>
 
 <template>
-  <p>About</p>
-  <ExampleComponent msg="example component's h1" />
+  <BasePage>
+    <h1>About me</h1>
+    <p>
+      A trained anthropologist, I learned to code in 2020, was hooked from the start, and am now
+      work as a full stack developer. While I love design and enjoy creative, responsive frontends,
+      I've discovered that my true passion is the backend: querying databases, using APIs,
+      automating processes. My projects encompass a bit of both worlds, but - apart from a small
+      site about coffee with a nice parallax effect (hopefully) that I plan to build - there will be
+      mostly data-related projects in the future.
+    </p></BasePage
+  >
 </template>
+
+<style scoped>
+h1 {
+  margin-bottom: 1rem;
+}
+</style>
